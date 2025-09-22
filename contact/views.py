8d14168b03c5d4096c4db548de40ceb0b234b5f8
yegeto <115154804+yegeto@users.noreply.github.com>
2025-09-22 22:20:18 +0300

    feat: create contact application

---
+++ contact/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+
+def contact_form(request):
+    context = {
+        'success': False,
+        'message': 'Contact form sent successfully.',
+    }
+    return JsonResponse(context)
+
+def contact(request):
+    return render(request, 'contact.html')
