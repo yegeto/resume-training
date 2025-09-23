85548add28fb3d0be5e6e478b47ea7fbd861a7ab
yegeto <115154804+yegeto@users.noreply.github.com>
2025-09-23 20:25:21 +0300

    refactor: apply experiences to frontend

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from core.models import GeneralSetting, ImageSetting, Skill
+from core.models import GeneralSetting, ImageSetting, Skill, Experience
 
 
 # Create your views here.
@@ -22,6 +22,9 @@ def index(request):
     # Skills
     skills = Skill.objects.all().order_by('order')
 
+    #Experiences
+    experiences = Experience.objects.all().order_by('-start_date')
+
     context = {
         'site_title': site_title,
         'site_keywords': site_keywords,
@@ -35,5 +38,6 @@ def index(request):
         'home_banner_image': home_banner_image,
         'site_favicon': site_favicon,
         'skills': skills,
+        'experiences': experiences,
     }
     return render(request, 'index.html', context=context)
