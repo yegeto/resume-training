092f3ff69a1ef2797114b4acb2cd10eae55cdc61
yegeto <115154804+yegeto@users.noreply.github.com>
2025-10-16 21:39:41 +0300

    refactor: s3 parameters

--- resume/settings.py
+++ resume/settings.py
@@ -144,7 +144,7 @@ else:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     AWS_DEFAULT_ACL = 'public-read'
-    AWS_S3_BUCKET_PARAMETERS = {
+    AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 01 Jan 2099 00:00:00 GMT',
         'CacheControl': 'max-age=94698000',
     }
